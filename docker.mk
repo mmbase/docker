c57db28abdeb7915a17b1831343b27e230d7ad69
Michiel Meeuwissen <git@meeuw.org>
2021-10-20 23:41:53 +0200

    Nicker making.

---
+++ docker.mk
@@ -0,0 +1,34 @@
+
+.INTERMEDIATE: %.md %.xml README.md
+.PHONY: explore
+
+docker: Dockerfile
+	docker build -t $(NAME):latest .
+	touch $@
+
+push: pushimage pushrm
+
+pushimage: docker
+	docker push $(NAME):latest
+	touch $@
+
+#https://github.com/christian-korneck/docker-pushrm
+pushrm: README.md docker
+	export DESCRIPTION=`docker inspect $(NAME) --format '{{ index .Config.Labels "org.mmbase.image.description"}}'` ; \
+	docker pushrm  $(NAME):latest --file $< -s "$${DESCRIPTION}"
+	touch $@
+
+%.xml: %.adoc
+	asciidoc -b docbook $<
+
+%.md: %.xml
+	pandoc -f docbook -t gfm $< -o $@
+
+explore: docker
+	docker run -it --entrypoint bash $(NAME)
+
+root: docker
+	docker run -it --entrypoint bash -u root $(NAME)
+
+clean:
+	rm -f docker pushrm pushimage README.xml README.md
